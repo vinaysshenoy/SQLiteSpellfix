da809c3d51ecda2d6f59c5fdca2a2bd3cb5fe792
Vinay S Shenoy <vinaysshenoy@gmail.com>
2018-07-23 11:21:04 +0530

    Substring search testing

--- app/src/main/java/com/vinaysshenoy/sqlitespellfix/sample/db/AppSqliteHelper.kt
+++ app/src/main/java/com/vinaysshenoy/sqlitespellfix/sample/db/AppSqliteHelper.kt
@@ -1,6 +1,5 @@
 package com.vinaysshenoy.sqlitespellfix.sample.db
 
-import android.content.ContentValues
 import android.content.Context
 import android.support.annotation.WorkerThread
 import android.util.Log
@@ -14,128 +13,113 @@ private const val LOG_TAG = "AppSqliteHelper"
 
 class AppSqliteHelper(context: Context) : SQLiteOpenHelper(context, "app.db", null, 1) {
 
-	override fun onCreate(db: SQLiteDatabase) {
-		Log.d(LOG_TAG, "On Create")
-		db.execSQL(
-				"""
+  override fun onCreate(db: SQLiteDatabase) {
+    Log.d(LOG_TAG, "On Create")
+    db.execSQL(
+        """
 			create table "Test" (
 				"id" text primary key,
-				"text1" text not null
+				"text1" text not null,
+        "searchText" text not null
 			)
 			""".trimIndent()
-		)
+    )
 
-		val data = listOf(
-				Item(UUID.randomUUID(), "Christmas is coming."),
-				Item(
-						UUID.randomUUID(), "If I don’t like something, I’ll stay away from it."
-				),
-				Item(
-						UUID.randomUUID(), "Let me help you with your baggage."
-				),
-				Item(
-						UUID.randomUUID(), "She advised him to come back at once."
-				),
-				Item(
-						UUID.randomUUID(), "We have never been to Asia, nor have we visited Africa."
-				),
-				Item(
-						UUID.randomUUID(), "The old apple revels in its authority."
-				),
-				Item(
-						UUID.randomUUID(), "A glittering gem is not enough."
-				),
-				Item(
-						UUID.randomUUID(),
-						"There were white out conditions in the town; subsequently, the roads were impassable."
-				)
-		)
-
-		data
-				.map {
-					ContentValues(2).apply {
-						put("id", it.id.toString())
-						put("text1", it.text)
-					}
-				}
-				.forEach {
-					db.insert("Test", null, it)
-				}
-
-		db.execSQL(
-				"""
+    db.execSQL(
+        """
 					create virtual table "Demo" using spellfix1
 				""".trimIndent()
-		)
+    )
 
-		db.execSQL(
-				"""
-			insert into "Demo"(word) select "text1" from "Test"
+
+    names
+        .map {
+          Item(
+              id = UUID.randomUUID(),
+              text = it
+          )
+        }
+        .map { it.toContentValues() }
+        .forEach {
+          db.insert("Test", null, it)
+        }
+
+    db.execSQL(
+        """
+			insert into "Demo"("word") select "searchText" from "Test"
 		""".trimIndent()
-		)
+    )
 
-	}
+  }
 
-	override fun onUpgrade(
-		db: SQLiteDatabase,
-		oldVersion: Int,
-		newVersion: Int
-	) {
-		TODO("not implemented")
-	}
+  override fun onUpgrade(
+      db: SQLiteDatabase,
+      oldVersion: Int,
+      newVersion: Int
+  ) {
+    TODO("not implemented")
+  }
 
-	override fun createConfiguration(
-		path: String?,
-		openFlags: Int
-	): SQLiteDatabaseConfiguration {
-		Log.d(LOG_TAG, "On Create Configuration")
-		val config = super.createConfiguration(path, openFlags)
+  override fun createConfiguration(
+      path: String?,
+      openFlags: Int
+  ): SQLiteDatabaseConfiguration {
+    Log.d(LOG_TAG, "On Create Configuration")
+    val config = super.createConfiguration(path, openFlags)
 
-		config.customExtensions.add(SQLiteCustomExtension("libspellfix3.so", "sqlite3_spellfix_init"))
+    config.customExtensions.add(SQLiteCustomExtension("libspellfix3.so", "sqlite3_spellfix_init"))
 
-		return config
-	}
+    return config
+  }
 
-	@WorkerThread
-	fun items(): List<ReadItem> {
-		return readableDatabase
-				.query(
-						"""
-			select * from "Test"
-		""".trimIndent()
-				)
-				.run {
-					generateSequence {
-						if (moveToNext()) {
-							ReadItem(
-									id = UUID.fromString(getString(getColumnIndex("id"))),
-									text = getString(getColumnIndex("text1")),
-									editDistance = 0
-							)
-						} else null
-					}.toList()
-				}
-	}
+  @WorkerThread
+  fun items(search: String = ""): List<ReadItem> {
+    return readableDatabase
+        .use {
+          it.query("""
+          select * from "Test" where "searchText" like '%$search%'
+        """.trimIndent()
+          ).run {
+            use {
+              generateSequence {
+                if (moveToNext()) {
+                  ReadItem(
+                      id = UUID.fromString(getString(getColumnIndex("id"))),
+                      text = getString(getColumnIndex("text1")),
+                      editDistance = 0
+                  )
+                } else null
+              }.toList()
+            }
+          }
+        }
 
-	@WorkerThread
-	fun itemsMatching(pattern: String): List<ReadItem> {
-		return readableDatabase
-				.query(
-						"""
+  }
+
+  @WorkerThread
+  fun itemsMatching(pattern: String): List<ReadItem> {
+    return readableDatabase
+        .use {
+          it
+              .query(
+                  """
 			select "word", editdist3('$pattern', "word") as "editdist" from "Demo" order by "editdist" asc
 		""".trimIndent()
-				)
-				.run {
-					generateSequence {
-						if (moveToNext()) {
-							ReadItem(
-//									id = UUID.fromString(getString(getColumnIndex("id"))),
-									id = UUID.randomUUID(),
-									text = getString(getColumnIndex("word")),
-									editDistance = getInt(getColumnIndex("editdist"))
-							)
-						} else null
-					}.toList()
-				}
-	}
+              )
+              .run {
+                use {
+                  generateSequence {
+                    if (moveToNext()) {
+                      ReadItem(
+                          //									id = UUID.fromString(getString(getColumnIndex("id"))),
+                          id = UUID.randomUUID(),
+                          text = getString(getColumnIndex("word")),
+                          editDistance = getInt(getColumnIndex("editdist"))
+                      )
+                    } else null
+                  }.toList()
+                }
+              }
+        }
+  }
 }
--- app/src/main/java/com/vinaysshenoy/sqlitespellfix/sample/db/Item.kt
+++ app/src/main/java/com/vinaysshenoy/sqlitespellfix/sample/db/Item.kt
@@ -1,14 +1,25 @@
 package com.vinaysshenoy.sqlitespellfix.sample.db
 
+import android.content.ContentValues
 import java.util.UUID
 
+val searchReplaceRegex = Regex("[\\s;_\\-:,]")
+
 data class Item(
-	val id: UUID,
-	val text: String
-)
+    val id: UUID,
+    val text: String
+) {
+
+  fun toContentValues() = ContentValues()
+      .apply {
+        put("id", id.toString())
+        put("text1", text)
+        put("searchText", text.replace(searchReplaceRegex, ""))
+      }
+}
 
 data class ReadItem(
-	val id: UUID,
-	val text: String,
-	val editDistance: Int
+    val id: UUID,
+    val text: String,
+    val editDistance: Int
 )
